0af92ed8d883c832e0c2610a0f888007de70014b
Kuba Nowak <kubanowak9755@gmail.com>
2022-07-31 22:11:54 +0200

    Initial commit

---
+++ db/migrate/20220730224145_create_devise_users.rb
@@ -0,0 +1,8 @@
+class CreateDeviseUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :devise_users do |t|
+      t.string :email, null: false
+      t.timestamps
+    end
+  end
+end
